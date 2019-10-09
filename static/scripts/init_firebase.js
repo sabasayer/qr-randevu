c5b810a5c8468c4b80a5c5d6d6523821f6b92cc6
sabasayer <45066901+sabasayer@users.noreply.github.com>
2019-10-09 14:27:51 +0300

    Update init_firebase.js

--- static/scripts/init_firebase.js
+++ static/scripts/init_firebase.js
@@ -1,12 +1,12 @@
  // Your web app's Firebase configuration
  var firebaseConfig = {
-    apiKey: "AIzaSyC2zZI8uqVDs4pXXrI32b8xh0H0BxSSn2E",
-    authDomain: "qr-randevu.firebaseapp.com",
-    databaseURL: "https://qr-randevu.firebaseio.com",
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
     projectId: "qr-randevu",
-    storageBucket: "qr-randevu.appspot.com",
-    messagingSenderId: "792607491535",
-    appId: "1:792607491535:web:dfa59712cdd724bf"
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
